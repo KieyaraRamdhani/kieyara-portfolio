f5819cb110edbdf3e64edb129a62fbf6ede1537f
Kieyara Ramdhani <ramdhanikieyara@gmail.com>
2026-07-16 22:50:38 +0200

    feat: create animated hero section

---
+++ JS/typing.js
@@ -0,0 +1,18 @@
+const typed = new Typed("#typing", {
+
+    strings: [
+        "Software Developer",
+        "AI Builder",
+        "Python Developer",
+        "C++ Programmer",
+        "Full Stack Developer",
+        "BSc IT Student"
+    ],
+
+    typeSpeed: 70,
+    backSpeed: 45,
+    backDelay: 1800,
+    loop: true,
+    smartBackspace: true
+
+});
